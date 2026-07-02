ef2e3c3e5c3ebcbb79fbe1877cab9a8b0832a524
unknown <rahmatillahniken@gmail.com>
2026-07-02 14:29:19 +0700

    new

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,11 @@
 require('dotenv').config();
-const mysql = require('mysql');
+const mysql = require('mysql2');
 
 const pool = mysql.createPool({
   connectionLimit: 10,
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASS || '', 
+  password: process.env.DB_PASS || '12345', 
   database: process.env.DB_NAME || 'dashboard_isp'
 });
 
